e9163a2cfaf5fdb0a5c3fcb1f48fe801d07c37f1
etnomiur123 <ruimonte_20@hotmail.com>
2020-11-14 21:34:33 +0000

    Added library menu

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React, { useState } from 'react';
-import Player from './components/Player'
-import Song from './components/Song'
-import './styles/app.scss';
-import data from './util';
+import React, { useState } from "react";
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
+import "./styles/app.scss";
+import data from "./util";
 
 function App() {
-
-  const [songs, setSongs] = useState(data())
-  const [currentSong, setCurrentSong] = useState(songs[0])
-  const [isPlaying, setIsPlaying] = useState(false)
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
-      <Song currentSong={currentSong}/>
-      <Player currentSong={currentSong} isPlaying={isPlaying} setIsPlaying={setIsPlaying}/>
+      <Song currentSong={currentSong} />
+      <Player
+        currentSong={currentSong}
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+      />
+      <Library currentSong={currentSong} songs={songs}/>
     </div>
   );
 }
